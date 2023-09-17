9d8e7a850e579b343fbd420b5c4e5b3ead969ba9
alanny-design <alannylucena@gmail.com>
2023-09-17 15:46:53 -0300

    frist commit

---
+++ src/Pages/components/footer/footer.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+import { Col, Container, Row } from "react-bootstrap";
+
+import "./footer.css";
+import {
+  InstagramIcon,
+  LinkedinIcon,
+  MenssagerIcon,
+  EmailIcon,
+} from "./socialIcons";
+import { Logo } from "../../../logo";
+import { useState } from "react";
+import { useEffect } from "react";
+
+export default function Footer() {
+  const [size, setSize] = useState({
+    x: window.innerWidth,
+    y: window.innerHeight
+  });
+  const updateSize = () =>
+    setSize({
+      x: window.innerWidth,
+      y: window.innerHeight
+    });
+  useEffect(() => (window.onresize = updateSize), []);
+  const mobileSize = 780;
+  return (
+    <Container fluid className="footer">
+      <Row>
+        <Col className={`text-left ${size.x <= mobileSize ? 'mobile' : ''}`} gab={3}>
+          <p>Follow |</p>
+          <InstagramIcon />
+          <LinkedinIcon />
+          <MenssagerIcon />
+          <EmailIcon />
+        </Col>
+        <Row>
+          <Col className="logo" xs={12}>
+            <Logo />
+            <p className="mx-0">© All Rights Reserved.</p>
+          </Col>
+        </Row>
+      </Row>
+    </Container>
+  );
+}
--- src/index.js
+++ src/index.js
@@ -1,17 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import {
+    createBrowserRouter,
+    RouterProvider,
+} from "react-router-dom";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+
+import './index.css';
+import Projects from './Pages/projects';
+
+
+
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <App />,
+    },
+    {
+        path: "/Projects",
+        element: <Projects />,
+    },
+    {
+        path: "/*",
+        element: <div>page not found, go to home:</div>,
+    },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+    <React.StrictMode>
+        <RouterProvider router={router} />
+    </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
